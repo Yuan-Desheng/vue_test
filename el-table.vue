814e2747a33ec8c999db30ecf0d89fd0f86104d4
yuandesheng <18603542180@163.com>
2023-09-27 16:29:54 +0800

    引入sortablejs组件,实现表格行拖拽

--- el-table.vue
+++ el-table.vue
@@ -1,9 +1,11 @@
 <template>
+    <!-- 对eltable设置一个名为draggable-table的class -->
     <el-table
-            :data="tableData"
-            border
-            row-key="date"
-            style="width: 100%"
+        :data="tableData"
+        border
+        row-key="date"
+        class="draggable-table"
+        style="width: 100%"
     >
         <el-table-column prop="date" label="日期" width="180"> </el-table-column>
         <el-table-column prop="name" label="姓名" width="180"> </el-table-column>
@@ -12,33 +14,33 @@
 </template>
 
 <script>
+import Sortable from "sortablejs";
 
 export default {
     data() {
         return {
             tableData: [
-                {
-                    date: "2016-05-01",
-                    name: "王小虎",
-                    address: "上海市普陀区金沙江路 1519 弄",
-                },
-                {
-                    date: "2016-05-02",
-                    name: "王小虎",
-                    address: "上海市普陀区金沙江路 1518 弄",
-                },
-                {
-                    date: "2016-05-03",
-                    name: "王小虎",
-                    address: "上海市普陀区金沙江路 1516 弄",
-                },
-                {
-                    date: "2016-05-04",
-                    name: "王小虎",
-                    address: "上海市普陀区金沙江路 1517 弄",
-                },
+                // 略去数据，与前段代码一直
             ],
         };
     },
+    mounted() {
+        // 需要支持拖动效果的列表容器，在这里我们设置为el-table组件的tbody，
+        // 注意：最前面的一段为前面el-table的class: draggable-table，主要为了防止如果页面有多个table，多个table同时实现拖拽效果
+        // 当然，如果多个table都需要拖拽效果，选择器中最前面的.draggable-table可以去除。
+        const tbody = document.querySelector(".draggable-table .el-table__body-wrapper tbody");
+        new Sortable(tbody, {
+            animation: 150,
+            // 需要在odEnd方法中处理原始eltable数据，使原始数据与显示数据保持顺序一致
+            onEnd: ({ newIndex, oldIndex }) => {
+                const targetRow = this.tableData[oldIndex];
+                this.tableData.splice(oldIndex, 1);
+                this.tableData.splice(newIndex, 0, targetRow);
+                console.table(this.tableData);
+            },
+        });
+    },
 };
 </script>
+<style scoped>
+</style>
